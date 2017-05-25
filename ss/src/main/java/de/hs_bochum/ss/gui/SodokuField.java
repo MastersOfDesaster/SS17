df0c139962dfe48d898e65be8652c0fd3a0f360c
Fabio Spiekermann <fspiekermann@itemis.de>
2017-05-25 15:01:15 +0200

    Ich komm in die PUSHen

--- ss/src/main/java/de/hs_bochum/ss/gui/SodokuField.java
+++ ss/src/main/java/de/hs_bochum/ss/gui/SodokuField.java
@@ -1,7 +1,22 @@
 package de.hs_bochum.ss.gui;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import de.hs_bochum.ss.model.Field;
 
 public class SodokuField extends JFrame{
-
+	
+	private Field field;
+	
+	private JTextField txtEmpty;
+	
+	public SodokuField() {
+		this.field = new Field();
+		
+	}
+	
+	public Field getField(){
+		return this.field;
+	}
 }
