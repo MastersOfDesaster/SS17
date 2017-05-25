5c9213d561ebd709a3d6bf701f7914da49e0835b
Fabio Spiekermann <fspiekermann@itemis.de>
2017-05-25 14:55:13 +0200

    Add unimplemented Methods

--- ss/src/main/java/de/hs_bochum/ss/algorithms/ThousandMonkeyAlgorithm.java
+++ ss/src/main/java/de/hs_bochum/ss/algorithms/ThousandMonkeyAlgorithm.java
@@ -1,10 +1,18 @@
 package de.hs_bochum.ss.algorithms;
 
 import de.hs_bochum.ss.interfaces.ISodokuSolver;
+import de.hs_bochum.ss.model.Field;
 
 public class ThousandMonkeyAlgorithm implements ISodokuSolver{
 
-	public void solve() {
+	@Override
+	public void solve(Field sudoku) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nextStep() {
 		// TODO Auto-generated method stub
 		
 	}
