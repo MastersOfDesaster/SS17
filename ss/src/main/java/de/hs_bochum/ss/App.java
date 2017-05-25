e67255e9ace732bac8932111ab39c8ee8841b1bb
Fabio Spiekermann <fspiekermann@itemis.de>
2017-05-25 11:45:27 +0200

    Init Structure

---
+++ ss/src/main/java/de/hs_bochum/ss/App.java
@@ -0,0 +1,13 @@
+package de.hs_bochum.ss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
