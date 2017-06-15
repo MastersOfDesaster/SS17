7154fada39f495a7d7f98ff6a8bdfd424e60d83a
Sebastian Lechte <sebastian.lechte@hs-bochum.de>
2017-06-15 17:16:02 +0200

    Mege gefixt

--- ss/src/main/java/de/hs_bochum/ss/App.java
+++ ss/src/main/java/de/hs_bochum/ss/App.java
@@ -2,41 +2,6 @@ package de.hs_bochum.ss;
 
 import de.hs_bochum.ss.control.GridValidator;
 import de.hs_bochum.ss.control.SudokuSolverControl;
-import de.hs_bochum.ss.modelNew.GridModel;
-import de.hs_bochum.ss.view.SudokuView;
-
-/**
- * Hello world!
- *
- */
-public class App {
-    public static void main( String[] args ) {
-    	init();
-    	start();
-    }
-
-
-
-	private static void init() {
-		SudokuView view = new SudokuView();
-    	GridModel model = new GridModel();
-    	GridValidator validator = new GridValidator(model);
-    	
-    	@SuppressWarnings("unused")
-		SudokuSolverControl control = new SudokuSolverControl(model, view, validator);
-	}
-	
-	private static void start() {
-		// TODO Auto-generated method stub
-		
-	}
-    
-    
-}
-package de.hs_bochum.ss;
-
-import de.hs_bochum.ss.control.GridValidator;
-import de.hs_bochum.ss.control.SudokuSolverControl;
 import de.hs_bochum.ss.view.SudokuView;
 import de.hs_bochum.ss.modelNew.GridModel;
 
